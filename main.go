82825d348515b4b53ec6979a1335279af5c97540
denkhaus <peristaltic@gmx.net>
2022-01-18 21:31:22 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,66 @@
+package flow
+
+import (
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+)
+
+type ErrorFunc func() error
+type chainTerminator struct {
+	message string
+	logger  *zap.Logger
+	args    []interface{}
+}
+
+func (p *chainTerminator) Error() string {
+	return p.message
+}
+
+func (p *chainTerminator) publishMessage() {
+	p.logger.Sugar().Infof(p.message, p.args...)
+}
+
+func TerminateChain(logger *zap.Logger, msg string, args ...interface{}) error {
+	return &chainTerminator{
+		message: msg,
+		logger:  logger,
+		args:    args,
+	}
+}
+
+func WrappedChainFunc(message string, fn ErrorFunc) ErrorFunc {
+	return func() error {
+		err := fn()
+		if chainTerm, ok := err.(*chainTerminator); ok {
+			chainTerm.publishMessage()
+			return nil
+		}
+
+		return errors.Wrap(err, message)
+	}
+}
+
+func Chain(fns ...func() error) error {
+	for _, fn := range fns {
+		if err := fn(); err != nil {
+			if chainTerm, ok := err.(*chainTerminator); ok {
+				chainTerm.publishMessage()
+				return nil
+			}
+
+			return err
+		}
+	}
+
+	return nil
+}
+
+func ChainWraped(fns ...func() (string, error)) error {
+	for _, fn := range fns {
+		if msg, err := fn(); err != nil {
+			return errors.Wrap(err, msg)
+		}
+	}
+
+	return nil
+}
